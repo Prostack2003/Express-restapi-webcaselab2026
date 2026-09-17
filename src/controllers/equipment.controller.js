efc5adca687f061c45707794c392694c3f210266
prostack2003 <cool.deds@mail.ru>
2026-09-17 16:05:16 +0300

    feat(equipment): add equipment CRUD endpoints

---
+++ src/controllers/equipment.controller.js
@@ -0,0 +1,50 @@
+import * as equipmentService from '../services/equipment.service.js';
+
+function createEquipment(request, response) {
+    const equipment = equipmentService.createEquipment(request.body);
+    response.location(`/api/equipment/${equipment.id}`);
+
+    return response.status(201).json({
+        data: equipment,
+    });
+}
+
+function updateEquipment(request, response) {
+    const id = request.params.id;
+    const equipment = equipmentService.updateEquipment(id, request.body);
+
+    return response.status(200).json({
+        data: equipment,
+    });
+}
+
+function deleteEquipment(request, response) {
+    const id = request.params.id;
+    equipmentService.deleteEquipment(id);
+
+    return response.status(204).send();
+}
+
+function listEquipment(request, response) {
+    const equipment = equipmentService.listEquipment();
+    return response.status(200).json({
+        data: equipment,
+    });
+}
+
+function getEquipmentById(request, response) {
+    const id = request.params.id;
+    const equipment = equipmentService.getEquipmentById(id);
+
+    return response.status(200).json({
+        data: equipment,
+    });
+}
+
+export {
+    createEquipment,
+    updateEquipment,
+    deleteEquipment,
+    listEquipment,
+    getEquipmentById,
+};
---
+++ src/routes/equipment/equipment.routes.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import {
+    createEquipment,
+    updateEquipment,
+    deleteEquipment,
+    listEquipment,
+    getEquipmentById,
+} from '../../controllers/equipment.controller.js';
+import {
+    validateCreateEquipmentBody,
+    validateUpdateEquipmentBody,
+    validateEquipmentIdParams,
+} from '../../middlewares/validate.middleware.js';
+
+const equipmentRouter = Router();
+
+equipmentRouter.get('/equipment', listEquipment);
+equipmentRouter.post(
+    '/equipment',
+    validateCreateEquipmentBody,
+    createEquipment
+);
+equipmentRouter.get(
+    '/equipment/:id',
+    validateEquipmentIdParams,
+    getEquipmentById
+);
+equipmentRouter.patch(
+    '/equipment/:id',
+    validateEquipmentIdParams,
+    validateUpdateEquipmentBody,
+    updateEquipment
+);
+equipmentRouter.delete(
+    '/equipment/:id',
+    validateEquipmentIdParams,
+    deleteEquipment
+);
+
+export { equipmentRouter };
